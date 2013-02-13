69303e66305778decce9212961668eb7ed048346
ddtabakova <dobrinkatabakova@yahoo.com>
2013-02-13 14:39:05 +0200

    Initial commit

---
+++ KFC/KFC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KFC
+//
+//  Created by Dobby on 2/13/13.
+//  Copyright (c) 2013 Dobby. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
